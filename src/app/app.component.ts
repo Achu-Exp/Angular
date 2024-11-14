7001753d1a3a69a7e4b5ee5d387ba4a0f0d64f12
Achu-Exp <achu73743@gmail.com>
2024-11-14 20:26:33 +0530

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./ui/navbar/navbar.component";
+import { CaroselofcardsComponent } from "./ui/caroselofcards/caroselofcards.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CaroselofcardsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
